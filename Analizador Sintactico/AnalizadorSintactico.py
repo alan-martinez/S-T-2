348183698b477aa276cc4b8e7628138d8028e6fa
Alan Jahir <alan.martinez5691@alumnos.udg.mx>
2022-09-12 19:35:06 -0500

    Todos los estados

--- Analizador Sintactico/AnalizadorSintactico.py
+++ Analizador Sintactico/AnalizadorSintactico.py
@@ -68,3 +68,433 @@ class estado(elementoPila):
     def __init__(self, cadena, tipo, pos, estado):
         elementoPila.__init__(self, cadena, tipo, pos)
         self.estado = estado
+
+class analizador:
+    def __init__(self, cadena_para_analizar):
+        self.cadena_analizada = cadena_para_analizar +"~"
+        self.edo = 0
+        self.i = 0
+        self.tmp =""
+        self.continua = True
+        self.tipo=list()
+        self.aux = 0
+        
+
+    def anlexico(self):
+        
+        while self.continua:
+            c = self.cadena_analizada[self.i]
+            
+            if self.edo == 0:                                                   #General
+                if c >= "0" and c <= "9":
+                    self.edo = 1
+                    self.tmp +=c
+                    
+                elif c == "E":
+                    self.tmp +=c
+                    self.tipo.append(3)
+                    objlex = noterminal("E","E",self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.continua = False
+                    
+                elif c >= "a" and c <= "z" or c >= "A" and c <= "Z" or c == "_":
+                    self.edo = 4
+                    self.tmp += c
+                elif c == " ":
+                    self.edo = 0
+                    #self.tmp +=c
+
+                elif c == "'" or c=='"':
+                    self.edo = 9
+                    self.tmp +=c
+
+                elif (c == "*") or (c == "/"):
+                    self.edo = 0
+                    self.tmp +=c
+                    self.tipo.append(6)
+                    objlex = terminal(self.tmp, 'Op. Mul', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.limpieza()
+
+                elif (c == "=") or (c == "!"):    
+                    self.limpieza()
+                    self.edo = 5
+                    self.tmp +=c
+                
+                elif (c == "<") or (c == ">"):    
+                    self.limpieza()
+                    self.edo = 6
+                    self.tmp +=c
+                elif (c == "|"):    
+                    self.limpieza()
+                    self.edo = 7
+                    self.tmp +=c
+                elif (c == "&"):    
+                    self.limpieza()
+                    self.edo = 8
+                    self.tmp +=c
+
+                
+                elif (c == "+") or (c == "-"):
+                    if self.aux == 1:
+                        self.tipo.append(5)
+                        objlex = terminal(self.tmp, 'Op. Suma', self.tipo[-1])
+                        listalexico.append(objlex)
+                        self.limpieza()
+
+                    elif self.aux == 2:
+                        
+                        self.limpieza()
+                    if c =="+":
+                        self.tmp +=c
+                        self.tipo.append(5)
+                        objlex = terminal(self.tmp, 'Op. Suma', self.tipo[-1])
+                        listalexico.append(objlex)
+                        self.limpieza()
+                        self.edo = 0
+                    else:
+                        self.tmp +=c
+                        self.tipo.append(5)
+                        objlex = terminal(self.tmp, 'Op. Suma', self.tipo[-1])
+                        listalexico.append(objlex)
+                        self.limpieza()
+                        self.edo = 0
+
+                elif c == ";":
+                    self.tmp +=c
+                    self.tipo.append(12)
+                    objlex = terminal(self.tmp, 'Punto y coma', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.edo = 0
+                
+                elif c == ",":
+                    self.tmp +=c
+                    self.tipo.append(13)
+                    objlex = terminal(self.tmp, 'Coma', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.edo = 0
+                    
+                
+                elif c == "$":
+                    self.tmp +=c
+                    self.tipo.append(23)
+                    objlex = terminal(self.tmp, 'Op. $', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.edo = 0
+                elif c == "(":
+                    self.tmp +=c
+                    self.tipo.append(14)
+                    objlex = terminal(self.tmp, 'Parentesis', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.edo = 0
+                    self.limpieza()
+                elif c == ")":
+                    self.tmp +=c
+                    self.tipo.append(15)
+                    objlex = terminal(self.tmp, 'Parentesis', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.edo = 0
+
+                elif c == "{":
+                    self.tmp +=c
+                    self.tipo.append(16)
+                    objlex = terminal(self.tmp, 'Corchete', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.edo = 0
+                    self.limpieza()
+                elif c == "}":
+                    self.tmp +=c
+                    self.tipo.append(17)
+                    objlex = terminal(self.tmp, 'Corchete', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.edo = 0
+                    
+
+                
+                elif c == "~":
+                    self.continua=False
+                
+            elif self.edo == 1:                                    
+                if c >= "0" and c <= "9":
+                    self.edo = 1
+                    self.tmp +=c
+
+                elif c == ".":
+                    self.edo = 2
+                    self.tmp += c
+
+                elif c == " ":
+                    self.edo = 0
+                    self.tipo.append(1)
+                    objlex = terminal(self.tmp, 'Entero', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.limpieza()
+                    #self.tmp +=c
+
+                elif c == "~":
+                    self.tipo.append(1)
+                    objlex = terminal(self.tmp, 'Entero', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.continua = False
+                else:
+                    self.edo = 0
+                    self.aux = 1
+                    self.i-=1
+
+            elif self.edo == 2: #Estado flotante                                
+                if c >= "0" and c <= "9":
+                    self.edo = 3
+                    self.tmp +=c
+            
+            elif self.edo == 3: #Estado termina en num                        
+                if c >= "0" and c <= "9":
+                    self.edo = 3
+                    self.tmp +=c
+                    self.tipo.append(2)
+                    objlex = terminal(self.tmp, 'Real', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.limpieza()
+                
+                elif c == " ":
+                    self.edo = 0
+                elif c == "~":
+                    
+                    self.continua = False
+                else:
+                    self.edo = 0
+                    self.i-=1
+
+            elif self.edo == 4: #Estado letras                                                                         
+                if c >= "a" and c <= "z" or c >= "A" and c <= "Z" or c == "_" or c >= "0" and c <= "9":
+                    self.edo = 4
+                    self.tmp +=c
+                elif c == " ":
+                    self.reservado()
+                    self.limpieza()
+                    self.edo = 0
+
+                elif c == "~":
+                    self.reservado()
+                    self.continua = False
+                else:
+                    self.reservado()
+                    self.edo = 0
+                    self.limpieza()
+                    self.i-=1
+                    self.aux=2
+            
+            elif self.edo == 5: #Estado terminacion de simbolo                             
+                if c == "=":
+                    self.edo = 0
+                    self.tmp +=c
+                    self.tipo.append(11)
+                    objlex = terminal(self.tmp, 'Op. Igualdad', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.limpieza()
+                
+                elif c == " ":
+                    self.edo = 0
+
+                elif c == "~":
+                    if self.cadena_analizada[self.i-1]=="=":
+                        self.edo = 0
+                        self.tipo.append(18)
+                        objlex = terminal(self.tmp, 'Op. Igual', self.tipo[-1])
+                        listalexico.append(objlex)
+                        self.limpieza()
+                    else:
+                        self.limpieza()
+                    self.continua = False
+                else:
+                    if self.cadena_analizada[self.i-1]=="=":
+                        self.edo = 0
+                        self.tipo.append(18)
+                        objlex = terminal(self.tmp, 'Op. Igual', self.tipo[-1])
+                        listalexico.append(objlex)
+                        self.limpieza()
+                    else:
+                        self.limpieza()
+                    self.i-=1
+
+            elif self.edo == 6:                            
+                if c == "=":
+                    self.edo = 0
+                    self.tmp +=c
+                    self.tipo.append(7)
+                    objlex = terminal(self.tmp, 'Op. Relacional', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.limpieza()
+                
+                elif c == " ":
+                    self.edo = 0
+
+                elif c == "~":
+                    self.edo = 0
+                    self.tipo.append(7)
+                    objlex = terminal(self.tmp, 'Op. Relacional', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.continua = False
+                else:
+                    self.edo = 0
+                    self.tipo.append(7)
+                    objlex = terminal(self.tmp, 'Op. Relacional', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.limpieza()
+                    self.i-=1
+
+            elif self.edo == 7:                                
+                if c == "|":
+                    self.edo = 0
+                    self.tmp +=c
+                    self.tipo.append(8)
+                    objlex = terminal(self.tmp, 'Op. Or', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.limpieza()
+                
+                elif c == " ":
+                    self.edo = 0
+
+                elif c == "~":
+                    
+                    self.continua = False
+                else:
+                    self.edo = 0
+                    self.limpieza()
+                    self.i-=1
+
+            elif self.edo == 8:              
+                if c == "&":
+                    self.edo = 0
+                    self.tmp +=c
+                    self.tipo.append(9)
+                    objlex = terminal(self.tmp, 'Op. And', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.limpieza()
+                
+                elif c == " ":
+                    self.edo = 0
+
+                elif c == "~":
+                    
+                    self.continua = False
+                else:
+                    self.edo = 0
+                    self.limpieza()
+                    self.i-=1
+            
+            elif self.edo == 9:                                
+                if c == "'" or c == '"':
+                    self.edo = 0
+                    self.tmp +=c
+                    self.tipo.append(3)
+                    objlex = terminal(self.tmp, 'Cadena', self.tipo[-1])
+                    listalexico.append(objlex)
+                    self.limpieza()
+                
+
+                elif c == "~":
+                    
+                    self.continua = False
+                else:
+                    self.edo = 9
+                    self.tmp +=c
+                    
+
+            self.i+=1
+
+        print(self.edo)
+        print(self.cadena_analizada)
+        print(self.tmp)
+        self.edo = 0
+        self.i = 0
+        self.tmp =""
+        self.continua = True
+
+    def reservado(self):
+        strid = self.tmp
+        if "while" == strid:
+            self.tipo.append(20)
+            objlex = terminal(self.tmp, 'Ciclo', self.tipo[-1])
+            listalexico.append(objlex)
+            print(strid, " Reservada Tipo", self.tipo[-1])
+        elif "if" == strid:
+            self.tipo.append(19)
+            objlex = terminal(self.tmp, 'Condicional', self.tipo[-1])
+            listalexico.append(objlex)
+            print(strid, " Reservada Tipo", self.tipo[-1])
+        elif "return" == strid:
+            self.tipo.append(21)
+            objlex = terminal(self.tmp, 'Retorno', self.tipo[-1])
+            listalexico.append(objlex)
+            print(strid, " Reservada Tipo", self.tipo[-1])
+        elif "else" == strid:
+            self.tipo.append(22)
+            objlex = terminal(self.tmp, 'Condicional', self.tipo[-1])
+            listalexico.append(objlex)
+            print(strid, " Reservada Tipo", self.tipo[-1])
+        elif "int" == strid:
+            self.tipo.append(4)
+            objlex = terminal(self.tmp, 'Tipo', self.tipo[-1])
+            listalexico.append(objlex)
+            print(strid, " Reservada Tipo", self.tipo[-1])
+        elif "float" == strid:
+            self.tipo.append(4)
+            objlex = terminal(self.tmp, 'Tipo', self.tipo[-1])
+            listalexico.append(objlex)
+            print(strid, " Reservada Tipo", self.tipo[-1])
+        elif "void" == strid:
+            self.tipo.append(4)
+            objlex = terminal(self.tmp, 'Tipo', self.tipo[-1])
+            listalexico.append(objlex)
+            print(strid, " Reservada Tipo", self.tipo[-1])
+        else:
+            self.tipo.append(0)
+            objlex = terminal(self.tmp, 'Identificador', self.tipo[-1])
+            listalexico.append(objlex)
+    def analizadorsintactico(self, i, auxelimna2, divcad2):
+        while True:
+            try:
+                fila = pila[-1].pos
+            except:
+                fila = pila[-1]
+            columna = buscar(divcad2[i])
+            accion = matrizreglas[fila][columna.pos]
+            accion= estado(str(accion), accion, accion, accion)
+            if accion.estado == 0:
+                print('Error')
+                break
+            elif accion.estado > 0:
+                i+=1
+                pila.append(columna)
+                pila.append(accion)
+                print('Desplazamiento', accion.cad)
+            elif accion.estado  <0:
+                if accion.estado == -1:
+                    print('Aceptado')
+                    break
+                else:
+                    for obj in lisreglas:
+                        if accion.estado == (obj.aux +1) * -1:
+                            print(obj.num, obj.regla)
+                            accion = matrizreglas[fila][obj.num]
+                            accion= estado(str(accion), accion, accion, accion)
+                            if obj.elementos !=0:
+                                eliminar = obj.elementos *2
+                                while eliminar != 0:
+                                    pila.pop()
+                                    eliminar-=1
+                                fila = pila[-1].pos
+                                accion = matrizreglas[fila][obj.num]   
+                                pila.append(obj.regla)
+                                pila.append(accion)
+                            else:
+                                pila.append(obj.regla)
+                                pila.append(accion)
+                            break
+                        
+    def limpieza(self):
+        self.edo = 0
+        self.tmp =""
+        self.continua = True
+
+    
